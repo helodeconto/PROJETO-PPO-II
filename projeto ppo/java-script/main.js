e0271ff9d1e4b65812fb602e546ab49405d239c6
Heloísa <helodc.online@gmail.com>
2022-05-25 20:19:06 -0300

    first commit

---
+++ projeto ppo/java-script/main.js
@@ -0,0 +1,5 @@
+/*function showContent() {
+    var template = document.getElementsByTagName("template")[0];
+    var clon = template.content.cloneNode(true);
+    document.body.appendChild(clon);
+}*/
